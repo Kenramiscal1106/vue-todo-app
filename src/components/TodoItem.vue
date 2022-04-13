5c3a68742821c5615f2f89a76b1b6a0c04676762
Kenramiscal1106 <ramiscalken@gmail.com>
2022-04-13 21:29:25 +0800

    Modified file: - added prop, functions, etc.

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,8 +1,25 @@
 <script lang="ts" setup>
+type todos = {
+  deadline?:string
+  done:boolean
+  id:number
+  text:string
+}
+  const props:Readonly<{
+    todoItems?:todos[],
+    todoItem?:todos
+  }> = defineProps(['todoItems', 'todoItem'])
+  props.todoItems
+  console.log({
+    todoItems:props.todoItems,
+    todoItem:props.todoItem
+  })
+  function removeSelf() {
+  }
 </script>
 
 <template>
-
+  <p>{{props.todoItem.text}}</p><button @click="">remove</button>
 </template>
 
 <style>
