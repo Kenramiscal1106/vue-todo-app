de09680d7d3da7fd3ffff2ad434439e84a86f851
Kenramiscal1106 <ramiscalken@gmail.com>
2022-04-13 14:30:57 +0800

    Added file:

---
+++ src/components/Todo.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>import { watch } from 'vue';
+
+  const props = defineProps(['todo', 'todoRef'])
+  console.log(props.todo)
+  watch(props.todoRef, () => {
+    console.log(props.todoRef.value)
+  })
+  props.todoRef.value = "This is some type of effect nothing special"
+</script>
+
+<template>
+
+</template>
+
+<style>
+
+
+</style>
