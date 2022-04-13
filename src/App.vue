a818e109413de8152860b96674641277f6a88705
Kenramiscal1106 <ramiscalken@gmail.com>
2022-04-13 21:28:52 +0800

    Modified file: - removed todoItem.vue - removed union type array  - added todoDeadline reactive state - added logic to determine whether or not the user has inputed - added remove todo function - added form inputs for deadline - added button to clear all todos - added key prop - added div container - added buttons to mark as complete/remove todo

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,72 @@
 <script setup lang="ts">
-  import { onMounted, ref, watch, watchEffect, type Ref } from 'vue';
+  import { onMounted, ref, watch, type Ref } from 'vue';
 type todos = {
   deadline?:string
   done:boolean
   id:number
   text:string
 }
-  const todoItems:Ref<todos[] | []> = ref([])
+  const todoItems:Ref<todos[]> = ref([])
   onMounted(() => {
     const todoLocalStorage:string | null = localStorage.getItem('todos');
     if (todoLocalStorage) {
       todoItems.value = JSON.parse(todoLocalStorage)
     }
   })
-  watch(todoItems, () => {
+  
+  const todoInput:Ref<string> = ref("")
+  const todoDeadline:Ref<string> = ref("")
+  function addTodo() {
+    if (todoInput.value != "") {
+      todoItems.value = [...todoItems.value, {
+          done:false,
+          id:Math.random() *1000,
+          text:todoInput.value,
+          deadline:todoDeadline.value
+        }] 
+      todoInput.value = ''
+      todoDeadline.value = ""
+      return
+    }
+    alert("you haven't entered an input")
+  }
+  function toggleDone(todoItemsArray:todos[], todoItem:todos):void {
+   todoItems.value =todoItemsArray.map((item) => {
+      if (item.id === todoItem.id) {
+        return {
+          id:item.id,
+          done:!todoItem.done,
+          text:item.text
+        }
+      }
+      return item 
+    })
+    todoItems.value = todoItems.value
+  }
+  function removeTodo(todoItemsArray:todos[], todoItem:todos):void {
+    todoItems.value = todoItemsArray.filter((item) => item.id !== todoItem.id)
+  }
+  watch(todoItems,  () => { 
     localStorage.setItem('todos', JSON.stringify(todoItems.value))
   })
-  const todoInput:Ref<string> = ref("")
-  function addTodo() {
-    todoItems.value = [
-      ...todoItems.value, {
-        done:false,
-        id:Math.random() *1000,
-        text:todoInput.value
-      }
-    ]
-  }
 </script>
 
 <template>
   <main>
     <form @submit.prevent="addTodo">
-      <input type="text" v-model="todoInput"><button type="submit">Submit</button>
+      <label for="text">Todo:</label><br>
+      <input type="text" v-model="todoInput" id="text"><br>
+      <label for="deadline">Due:</label><br>
+      <input type="text" id="deadline" v-model="todoDeadline">
+      <button type="submit">Submit</button>
     </form>
+      <button @click="() => {todoItems = []}">clear Todos</button>
+
     <div v-if="todoItems.length !== 0">
-      <div v-for="todoItem in todoItems">
-        <p>{{todoItem.text}}</p>
+      <div v-for="todoItem in todoItems" :key="todoItem.id">
+        <div>
+          <p>{{todoItem.text}}</p><button @click="() => {toggleDone(todoItems, todoItem)}">{{todoItem.done ? "undo" : "mark as done"}}</button> <button @click="removeTodo(todoItems, todoItem)">Remove</button>
+        </div>
       </div>
     </div>
     <div v-else>
