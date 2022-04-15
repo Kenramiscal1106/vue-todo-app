918e642fb05e8b5694de821d3b5f2d7918fa4ee2
Kenramiscal1106 <ramiscalken@gmail.com>
2022-04-15 20:57:50 +0800

    Modified file: - template changes

--- src/App.vue
+++ src/App.vue
@@ -146,13 +146,14 @@ type date = {
     <form @submit.prevent="addTodo" autocomplete="off">
       <input type="text" v-model="todoInput" id="text" placeholder="Todo:">
       <input type="text" id="deadline" v-model="todoDeadline" placeholder="Due:">
+      <button type="submit">&plus;</button>
+      <br>
       <template v-if="todoDeadline !== ''">
         <label v-if="isValidDeadline">{{message}}</label>
         <label v-else-if="!isValidDeadline">
           invalid property value; the time format must be: HH:MM AM/PM (12-hour) or HH:MM (24-hour)
         </label>
       </template>
-      <button type="submit">&plus;</button>
     </form>
       <button @click="() => {todoItems = []}">clear Todos</button>
 
@@ -160,7 +161,10 @@ type date = {
       <div class="m-2">
         <template v-for="todoItem in todoItems" :key="todoItem.id">
           <div class="todo-item">
-            <div :class= "todoItem.done ? 'flex-1 done' : 'flex-1 undone' ">{{todoItem.text}} <span v-if="pastDeadline(todoItem.deadline, currentTime().hour24) && !todoItem.done" class="text-red-600">Past due date</span></div><div v-if="todoItem.deadline !== ''">
+            <div :class= "todoItem.done ? 'flex-1 done flex gap-3' : 'flex-1 undone flex gap-3' ">{{todoItem.text}} <span v-if="pastDeadline(todoItem.deadline, currentTime().hour24) && !todoItem.done" class="text-red-600 flex items-center"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-alarm" viewBox="0 0 16 16">
+  <path d="M8.5 5.5a.5.5 0 0 0-1 0v3.362l-1.429 2.38a.5.5 0 1 0 .858.515l1.5-2.5A.5.5 0 0 0 8.5 9V5.5z"/>
+  <path d="M6.5 0a.5.5 0 0 0 0 1H7v1.07a7.001 7.001 0 0 0-3.273 12.474l-.602.602a.5.5 0 0 0 .707.708l.746-.746A6.97 6.97 0 0 0 8 16a6.97 6.97 0 0 0 3.422-.892l.746.746a.5.5 0 0 0 .707-.708l-.601-.602A7.001 7.001 0 0 0 9 2.07V1h.5a.5.5 0 0 0 0-1h-3zm1.038 3.018a6.093 6.093 0 0 1 .924 0 6 6 0 1 1-.924 0zM0 3.5c0 .753.333 1.429.86 1.887A8.035 8.035 0 0 1 4.387 1.86 2.5 2.5 0 0 0 0 3.5zM13.5 1c-.753 0-1.429.333-1.887.86a8.035 8.035 0 0 1 3.527 3.527A2.5 2.5 0 0 0 13.5 1z"/>
+</svg> Past due date</span></div><div v-if="todoItem.deadline !== ''">
             <audio>
               <source src="./assets/mixkit-facility-alarm-908.wav" type="audio/wav"/>
             </audio>Due: {{todoItem.deadline}}</div><button @click="() => {toggleDone(todoItems, todoItem)}">{{todoItem.done ? "❌" : "✅"}}</button> <button @click="removeTodo(todoItems, todoItem)">❌</button>
