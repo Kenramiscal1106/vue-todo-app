d1aff7c7d1b53bff6bf1aa8cdcb9328b19d1910e
Kenramiscal1106 <ramiscalken@gmail.com>
2022-04-15 21:24:24 +0800

    Modified file: - added max-width class - removed redundant use cases for the classes

--- src/App.vue
+++ src/App.vue
@@ -158,10 +158,10 @@ type date = {
       <button @click="() => {todoItems = []}">clear Todos</button>
 
     <template v-if="todoItems.length !== 0">
-      <div class="m-2">
+      <div class="m-2 max-w-[960px]">
         <template v-for="todoItem in todoItems" :key="todoItem.id">
           <div class="todo-item">
-            <div :class= "todoItem.done ? 'flex-1 done flex gap-3' : 'flex-1 undone flex gap-3' ">{{todoItem.text}} <span v-if="pastDeadline(todoItem.deadline, currentTime().hour24) && !todoItem.done" class="text-red-600 flex items-center"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-alarm" viewBox="0 0 16 16">
+            <div :class= "todoItem.done ? 'done' : (!pastDeadline(todoItem.deadline, currentTime().hour24) ? 'undone' : 'past-deadline') ">{{todoItem.text}} <span v-if="pastDeadline(todoItem.deadline, currentTime().hour24) && !todoItem.done" class="text-red-600 flex items-center ml-auto"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-alarm" viewBox="0 0 16 16">
   <path d="M8.5 5.5a.5.5 0 0 0-1 0v3.362l-1.429 2.38a.5.5 0 1 0 .858.515l1.5-2.5A.5.5 0 0 0 8.5 9V5.5z"/>
   <path d="M6.5 0a.5.5 0 0 0 0 1H7v1.07a7.001 7.001 0 0 0-3.273 12.474l-.602.602a.5.5 0 0 0 .707.708l.746-.746A6.97 6.97 0 0 0 8 16a6.97 6.97 0 0 0 3.422-.892l.746.746a.5.5 0 0 0 .707-.708l-.601-.602A7.001 7.001 0 0 0 9 2.07V1h.5a.5.5 0 0 0 0-1h-3zm1.038 3.018a6.093 6.093 0 0 1 .924 0 6 6 0 1 1-.924 0zM0 3.5c0 .753.333 1.429.86 1.887A8.035 8.035 0 0 1 4.387 1.86 2.5 2.5 0 0 0 0 3.5zM13.5 1c-.753 0-1.429.333-1.887.86a8.035 8.035 0 0 1 3.527 3.527A2.5 2.5 0 0 0 13.5 1z"/>
 </svg> Past due date</span></div><div v-if="todoItem.deadline !== ''">
