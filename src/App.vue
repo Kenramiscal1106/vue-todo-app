bd306d4a49cc619f2576a78b5c0f3a3cbe4e3ec2
Kenramiscal1106 <ramiscalken@gmail.com>
2022-05-09 01:40:50 +0800

    Modified file: - changed the conditional logic of hour - changed the conditional logic of twelveHourSplit - changed timeString return value to this.propreties - added currentTimeObj state variable - removed toggleDone - removed pastDeadline - moved elements to TodoItems.vue

--- src/App.vue
+++ src/App.vue
@@ -17,14 +17,14 @@ type Date = {
   function currentTime():{hour12:Date, hour24:Date} {
     const time = new Date();
     const hour12:Date = {
-      hour: (time.getHours() > 12 ? time.getHours() - 12 : time.getHours()),
+      hour: (time.getHours() > 12 ? time.getHours() - 12 : time.getHours() === 0 ? 12 : time.getHours()),
       minute:time.getMinutes(),
       seconds:time.getSeconds(),
       twelveHourSplit:function() {
-        return (time.getHours() > 12 ? "PM" : "AM")
+        return (time.getHours() < 12 ? "AM" : "PM")
       },
       timeString:function() {
-        return `${(time.getHours() > 12 ? time.getHours() - 12 : time.getHours())}:${time.getMinutes()}:${time.getSeconds()} ${typeof this.twelveHourSplit !== "undefined" ? this.twelveHourSplit() : ""}`
+        return `${this.hour}:${this.minute < 10 ? `0${this.minute}`: this.minute}:${this.seconds < 10 ? `0${this.seconds}` : this.seconds} ${typeof this.twelveHourSplit !== "undefined" ? this.twelveHourSplit() : ""}`
       }
     }
     const hour24:Date = {
@@ -40,9 +40,11 @@ type Date = {
       hour24
     }
   }
+  const currentTimeObj:Ref<Date> = ref(currentTime().hour24);;
   const time:Ref<string> = ref(currentTime().hour12.timeString());
   setInterval(async () => {
     time.value = currentTime().hour12.timeString()
+    currentTimeObj.value = currentTime().hour24
   }, 1000)
   const todoItems = reactive<{value:Todo[]}>({value:[]})
   onMounted(() => {
@@ -94,22 +96,6 @@ type Date = {
       alert("You haven't entered any 😠")
     }
   }
-  function toggleDone(todoItemsArray:Todo[], todoItem:Todo):void {
-   todoItems.value =todoItemsArray.map((item) => {
-      if (item.id === todoItem.id) {
-        return {
-          id:item.id,
-          done:!todoItem.done,
-          text:item.text,
-          deadline:item.deadline
-        }
-      }
-      return item 
-    })
-  }
-  function removeTodo(todoItemsArray:Todo[], todoItem:Todo):void {
-    todoItems.value = todoItemsArray.filter((item) => item.id !== todoItem.id)
-  }
   watch(todoItems,  () => { 
     localStorage.setItem('todos', JSON.stringify(todoItems.value))
   })
@@ -121,51 +107,6 @@ type Date = {
     }
   })
   const message = "valid"
-  function pastDeadline(todoDeadline:string, currentTime:Date):boolean {
-    if (todoDeadline === "") {
-      return false
-    }
-    function convertTo24HourFormat(todoDeadline:string):string {
-      const AMorPM = todoDeadline.match(/(A|P)/gi)?.join()
-      const removedAMPM = todoDeadline.replace(/(A|P)m$/gi, '');
-      const time = removedAMPM.match(/\d+/gi)
-      let hour, minute
-      if (time && typeof AMorPM !== 'undefined') {
-        hour = parseInt(time[0]);
-        minute = time[1]
-        hour = (AMorPM.toLowerCase() === 'p' && hour !== 12 ? hour + 12 : hour)
-      } else {
-        throw new Error('Parsing error')
-      }
-      return `${hour}:${minute}`
-    }
-    function decompose(todoDeadline:string):{hour:number, minute:number, totalMinutes:number} {
-      let hour, minute;
-      const hourMinuteArray= todoDeadline.match(/\d+/gi)
-      if (hourMinuteArray) {
-        [hour, minute] = hourMinuteArray
-      }
-      if (typeof hour !== 'undefined' && typeof minute !== "undefined") {
-        const totalMinutes = parseInt(hour) * 60 + parseInt(minute);
-        return {
-          hour:parseInt(hour),
-          minute:parseInt(minute),
-          totalMinutes
-        }
-      } 
-      return {
-        hour:NaN,
-        minute:NaN,
-        totalMinutes:NaN
-      }
-    }
-    const convertedFormat = (todoDeadline.search(/(A|P)m$/gi) !== -1 ? convertTo24HourFormat(todoDeadline) : todoDeadline)
-    const decomposeConvertedFormat = decompose(convertedFormat)
-    if ((currentTime.hour * 60) + currentTime.minute >= decomposeConvertedFormat.totalMinutes) {
-      return true
-    }
-    return false
-  }
 </script>
 
 <template>
@@ -189,31 +130,7 @@ type Date = {
     <template v-if="todoItems.value.length !== 0">
       <div class="m-2 max-w-[960px] gap-1 flex flex-col">
         <template v-for="todoItem in todoItems.value" :key="todoItem.id">
-          <TodoItems v-model:todos="todoItems" :specific-todo-item="todoItem"/>
-            <button :class="`flex-1 text-left mark-as-done-button todo-item ${todoItem.done ? 'todo-is-done ' : ''}`" @click="toggleDone(todoItems.value, todoItem)">
-              <input type="checkbox" name="" id="" v-model="todoItem.done">{{todoItem.text}} 
-              <div class="todo-item-side">
-                <span v-if="pastDeadline(todoItem.deadline, currentTime().hour24) && !todoItem.done" class="text-red-600 flex items-center ml-auto"><img src="./assets/alarm.svg" alt=""> 
-                  Past due Date
-                  <audio autoplay loop><source src="./assets/mixkit-facility-alarm-908.wav" type="audio/wav"/></audio>
-                </span>
-                <span v-if="todoItem.deadline !== ''">Deadline: {{todoItem.deadline}}</span>
-                <span class="option-buttons">
-                  <button @click="removeTodo(todoItems.value, todoItem)" class="remove-button">
-                    <svg width="14" height="14" viewBox="0 0 37 37" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M33.2306 33.2306L3.76933 3.76936M3.76933 33.2306L33.2306 3.76936" stroke="currentColor" stroke-width="7"/>
-    </svg>
-                  </button>
-                  <button>
-                    <svg width="14" height="14" viewBox="0 0 37 37" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <circle cx="7.5" cy="18.5" r="4.5" fill="currentColor"/>
-                      <circle cx="18.5" cy="18.5" r="4.5" fill="currentColor"/>
-                      <circle cx="29.5" cy="18.5" r="4.5" fill="currentColor"/> 
-                    </svg>
-                  </button>
-                </span>
-            </div>
-            </button>
+          <TodoItems v-model:todos="todoItems" :specific-todo-item="todoItem" :current-time-ref="currentTimeObj"/>
 
         </template>
       </div>
