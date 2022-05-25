3dd004f7f04d589ceccf2f83b1ee0c95829d0972
Kenramiscal1106 <ramiscalken@gmail.com>
2022-05-25 14:14:53 +0800

    - added reactive variable selectedTodo - added condition and guard clauses - added a new function updateTodo - changed tag, templates, and styling - added form for updating

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
   const currentTimeObj:Ref<utils.Date> = ref(utils.currentTime().hour24);
   const time:Ref<string> = ref(utils.currentTime().hour12.timeString());
   const todoItems = reactive<{value:utils.Todo[]}>({value:[]})
+  const selectedTodo = reactive<{value:null | utils.Todo}>({value: null})
   const isValidDeadline:Ref<boolean> = ref(true);
   const todoInput:Ref<string> = ref("")
   const todoDeadline:Ref<string> = ref("");
@@ -58,15 +59,26 @@
       }
       return 0
     })
-    thereIsDeadline.value = sumOfDeadline.reduce((prev, next, i, arr):any => {
-      return prev + next
-    })
+    if (sumOfDeadline.length !== 0) {
+      thereIsDeadline.value = sumOfDeadline.reduce((prev, next, i, arr):any => {
+        return prev + next
+      })
+      return
+    }
+    thereIsDeadline.value = 0
   })
+  function updateTodo() {
+    const todoIds = todoItems.value.map((todoItem) => todoItem.id)
+    const selectedTodoIndex = todoIds.indexOf(selectedTodo.value ? selectedTodo.value.id : -1)
+    todoItems.value[selectedTodoIndex] = selectedTodo.value ? selectedTodo.value : todoItems.value[selectedTodoIndex - 1]
+    todoItems.value = todoItems.value
+    selectedTodo.value = null
+  }
 </script>
 
 <template>
   <main class="p-4">
-    <h2>{{time}}</h2>
+    <h1>{{time}}</h1>
     <div :class="'fixed top-1 left-[50%] -translate-x-[50%] bg-red-600 text-white px-4 py-2 rounded-md transition-opacity duration-300 ' + (thereIsDeadline === 0 ? ' opacity-0' : '') ">
       <h3>Warning: there are some todos that are past deadline</h3>
       <span v-if="thereIsDeadline > 0">
@@ -78,7 +90,7 @@
     <form @submit.prevent="addTodo" autocomplete="off">
       <input type="text" v-model="todoInput" id="text" placeholder="Todo:">
       <input type="text" id="deadline" v-model="todoDeadline" placeholder="Due:">
-      <button type="submit">&plus;</button>
+      <button type="submit" class="bg-green-500 hover:bg-green-600 text-white override">add todo</button>
       <br>
       <template v-if="todoDeadline !== ''">
         <label v-if="isValidDeadline">valid</label>
@@ -87,13 +99,20 @@
         </label>
       </template>
     </form>
+    <div v-if="selectedTodo.value" class="flex items-center justify-center absolute top-0 left-0 w-full h-full bg-gray-600 bg-opacity-30">
+      <form autocomplete="off" @submit.prevent="updateTodo">
+        <input type="text" v-model="selectedTodo.value.text">
+        <input type="text" v-model="selectedTodo.value.deadline">
+        <button type="submit">Update</button>
+      </form>
+    </div>
     <button @click="() => {todoItems.value = utils.markAllAsUndone(todoItems)}">Mark all as undone</button>
     <button @click="() => {todoItems.value = utils.markAllAsDone(todoItems)}">Mark all as Done</button>
     <button @click="() => {todoItems.value = []}">clear Todo</button>
     <template v-if="todoItems.value.length !== 0">
       <div class="m-2 max-w-[960px] gap-1 flex flex-col">
         <template v-for="todoItem in todoItems.value" :key="todoItem.id">
-          <TodoItems v-model:todos="todoItems" :specific-todo-item="todoItem" :current-time-ref="currentTimeObj"/>
+          <TodoItems v-model:todos="todoItems" v-model:selected-todo="selectedTodo" :specific-todo-item="todoItem" :current-time-ref="currentTimeObj"/>
 
         </template>
       </div>
