224ddf05c347df7e8093840ac157943361401fa2
Kenramiscal1106 <ramiscalken@gmail.com>
2022-05-25 14:16:07 +0800

    - removed unused imports - added new field for Props - template change and styling change

--- src/components/TodoItems.vue
+++ src/components/TodoItems.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import {pastDeadline, type Date} from "../lib/utils"
+import { pastDeadline } from "../lib/utils";
 
 interface Props {
   todos: { value: Todo[] };
   specificTodoItem: Todo;
-  currentTimeRef:any
+  currentTimeRef: any;
+  selectedTodo: { value: Todo | null };
 }
 type Todo = {
   deadline: string;
@@ -33,54 +33,82 @@ function removeTodo(todoItemsArray: Todo[], todoItem: Todo): void {
 </script>
 
 <template>
-  <button
-    :class="`flex-1 text-left mark-as-done-button todo-item ${
-      specificTodoItem.done ? 'todo-is-done ' : ''
-    } ${pastDeadline(specificTodoItem.deadline, currentTimeRef) && !specificTodoItem.done ? 'deadline' : ''}`"
-    @click="toggleDone(todos.value, specificTodoItem)"
-  >
-    <input type="checkbox" name="" id="" v-model="specificTodoItem.done" />{{
-      specificTodoItem.text
-    }}
-    <div class="todo-item-side">
-      <span v-if="specificTodoItem.deadline !== ''"
-        >Deadline: {{ specificTodoItem.deadline }}</span
-      >
-      <span class="option-buttons">
-        <button
-          @click="removeTodo(todos.value, specificTodoItem)"
-          class="remove-button"
+  <div class="flex todo-item">
+    <button
+      :class="`flex-1 text-left mark-as-done-button todo-text ${
+        specificTodoItem.done ? 'todo-is-done ' : ''
+      } ${
+        pastDeadline(specificTodoItem.deadline, currentTimeRef) &&
+        !specificTodoItem.done
+          ? 'deadline'
+          : ''
+      }`"
+      @click="toggleDone(todos.value, specificTodoItem)"
+    >
+      <input type="checkbox" name="" id="" v-model="specificTodoItem.done" />{{
+        specificTodoItem.text
+      }}
+      <div class="todo-text-side">
+        <span v-if="specificTodoItem.deadline !== ''"
+          >Deadline: {{ specificTodoItem.deadline }}</span
         >
-          <svg
-            width="14"
-            height="14"
-            viewBox="0 0 37 37"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M33.2306 33.2306L3.76933 3.76936M3.76933 33.2306L33.2306 3.76936"
-              stroke="currentColor"
-              stroke-width="7"
-            />
-          </svg>
-        </button>
-        <button>
-          <svg
-            width="14"
-            height="14"
-            viewBox="0 0 37 37"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <circle cx="7.5" cy="18.5" r="4.5" fill="currentColor" />
-            <circle cx="18.5" cy="18.5" r="4.5" fill="currentColor" />
-            <circle cx="29.5" cy="18.5" r="4.5" fill="currentColor" />
-          </svg>
-        </button>
-      </span>
+      </div>
+    </button>
+    <div class="option-buttons">
+      <button
+        @click="removeTodo(todos.value, specificTodoItem)"
+        class="remove-button"
+      >
+        <svg
+          width="14"
+          height="14"
+          viewBox="0 0 37 37"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M33.2306 33.2306L3.76933 3.76936M3.76933 33.2306L33.2306 3.76936"
+            stroke="currentColor"
+            stroke-width="7"
+          />
+        </svg>
+      </button>
+      <button @click="props.selectedTodo.value = specificTodoItem">
+        <svg
+          width="14"
+          height="14"
+          viewBox="0 0 37 37"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <circle cx="7.5" cy="18.5" r="4.5" fill="currentColor" />
+          <circle cx="18.5" cy="18.5" r="4.5" fill="currentColor" />
+          <circle cx="29.5" cy="18.5" r="4.5" fill="currentColor" />
+        </svg>
+      </button>
     </div>
-  </button>
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+  .todo-item {
+    &:hover > button.todo-text {
+      @apply bg-gray-300;
+    }
+    > button.todo-text > .todo-text-side {
+      margin-left:auto;
+    }
+    &:hover > .option-buttons {
+      opacity:1;
+    }
+    > .option-buttons {
+      display:flex;
+      align-items:center;
+      opacity:0;
+      >.remove-button {
+        background:red;
+        color:white;
+      }
+    }
+  }
+</style>
