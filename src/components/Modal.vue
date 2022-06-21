cb7657f0feccfbef2ef0d9af58a90e9b7263eb6e
Kenramiscal1106 <ramiscalken@gmail.com>
2022-06-21 10:22:14 +0800

    created file

---
+++ src/components/Modal.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div class="absolute top-0 w-full h-full bg-white bg-opacity-5 backdrop-blur-sm grid place-content-center">
+    <slot></slot>
+  </div>
+</template> 
+
+<style>
+</style>
