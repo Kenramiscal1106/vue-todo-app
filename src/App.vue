349c08fcd92b2fd4d6f1f8f8b77dbcf490a7c0e3
Kenramiscal1106 <ramiscalken@gmail.com>
2022-06-21 10:26:59 +0800

    - added TodoForm component - added modal component - removed state for the todoform - added state for the modal component - removed functions and state watchers for state of the todoform - removed utility classes from elements - added component in template - removed todoForm elements in the app.vue file - removed template elements for the todoItems

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <script setup lang="ts">
 import { onMounted, reactive, ref, watch, type Ref } from 'vue';
+import TodoForm from './components/TodoForm.vue';
 import TodoItems from './components/TodoItems.vue'
+import Modal from './components/Modal.vue'
 import * as utils from "./lib/utils"
 const currentTimeObj: Ref<utils.Date> = ref(utils.currentTime().hour24);
 const time: Ref<string> = ref(utils.currentTime().hour12.timeString());
 const todoItems = reactive<{ value: utils.Todo[] }>({ value: [] })
 const selectedTodo = reactive<{ value: null | utils.Todo }>({ value: null })
-const isValidDeadline: Ref<boolean> = ref(true);
-const todoInput: Ref<string> = ref("")
-const todoDeadline: Ref<string> = ref("");
 const thereIsDeadline = ref<number>(0);
-const DeadlinePattern = /(^(1[0-2]|0?[0-9]):[0-5][0-9] ?(P|A)m$)|(^([0-2][0-3]|0?[1-9]):[0-5][0-9] ?$)/ig;
+const modalOpen = ref<boolean>(false)
 setInterval(async () => {
   time.value = utils.currentTime().hour12.timeString()
   currentTimeObj.value = utils.currentTime().hour24
@@ -21,35 +20,8 @@ onMounted(() => {
     todoItems.value = JSON.parse(todoLocalStorage)
   }
 })
-function addTodo() {
-  if (todoInput.value != "" && isValidDeadline.value) {
-    todoItems.value = [...todoItems.value, {
-      done: false,
-      id: Math.random() * 1000,
-      text: todoInput.value,
-      deadline: todoDeadline.value
-    }]
-    todoInput.value = ''
-    todoDeadline.value = ""
-    return
-  }
-  else if (!isValidDeadline.value) {
-    alert("invalid time format")
-    return
-  }
-  else {
-    alert("You haven't entered any 😠")
-  }
-}
 watch(todoItems, () => {
   localStorage.setItem('todos', JSON.stringify(todoItems.value))
-})
-watch(todoDeadline, () => {
-  if (todoDeadline.value.search(DeadlinePattern) !== -1 || todoDeadline.value === "") {
-    isValidDeadline.value = true
-  } else {
-    isValidDeadline.value = false
-  }
 })
 watch([todoItems.value, currentTimeObj], () => {
   const sumOfDeadline = todoItems.value.map((todoItem) => {
@@ -77,10 +49,14 @@ function updateTodo() {
 </script>
 
 <template>
-  <main class="p-4 flex flex-col items-center max-w-3xl">
+  <main class="">
     <h1>{{ time }}</h1>
+    <Modal v-if="modalOpen">
+      <TodoForm v-model:todo-items="todoItems" />
+    </Modal>
+    <button @click="() => modalOpen = !modalOpen" v-if="modalOpen" class="absolute right-4 top-4 bg-slate-600 hover:bg-slate-900 text-white p-2 z-40">Close add todo</button>
     <div
-      :class="'fixed top-1 left-[50%] -translate-x-[50%] bg-red-600 text-white px-4 py-2 rounded-md transition-opacity duration-300 ' + (thereIsDeadline === 0 ? ' opacity-0' : '')">
+      class="">
       <h3>Warning: there are some todos that are past deadline</h3>
       <span v-if="thereIsDeadline > 0">
         <audio autoplay loop>
@@ -88,25 +64,8 @@ function updateTodo() {
         </audio>
       </span>
     </div>
-    <form @submit.prevent="addTodo" autocomplete="off" class="px-2 py-4 bg-gray-200 flex flex-col gap-2 w-full">
-      <div>
-        <input type="text" v-model="todoInput" id="text" placeholder="Todo:">
-      </div>
-      <div>
-        <input type="text" id="deadline" v-model="todoDeadline" placeholder="Due:">
-      </div>
-      <div>
-        <button type="submit" class="bg-green-500 hover:bg-green-600 text-white override">add todo</button>
-      </div>
-      <template v-if="todoDeadline !== ''">
-        <div v-if="isValidDeadline">valid</div>
-        <div v-else-if="!isValidDeadline">
-          invalid property value; the time format must be: HH:MM AM/PM (12-hour) or HH:MM (24-hour)
-        </div>
-      </template>
-    </form>
     <div v-if="selectedTodo.value"
-      class="flex items-center justify-center absolute top-0 left-0 w-full h-full bg-gray-600 bg-opacity-30">
+      class="">
       <form autocomplete="off" @submit.prevent="updateTodo">
         <input type="text" v-model="selectedTodo.value.text">
         <input type="text" v-model="selectedTodo.value.deadline">
@@ -118,17 +77,7 @@ function updateTodo() {
       <button @click="() => { todoItems.value = utils.markAllAsDone(todoItems) }">Mark all as Done</button>
       <button @click="() => { todoItems.value = [] }">clear Todo</button>
     </div>
-    <template v-if="todoItems.value.length !== 0">
-      <div class="m-2 w-full gap-1 flex flex-col items-stretch">
-        <template v-for="todoItem in todoItems.value" :key="todoItem.id">
-          <TodoItems v-model:todos="todoItems" v-model:selected-todo="selectedTodo" :specific-todo-item="todoItem"
-            :current-time-ref="currentTimeObj" />
-
-        </template>
-      </div>
-    </template>
+    <TodoItems :current-time-obj="currentTimeObj" v-model:todo-items="todoItems" v-model:selected-todo="selectedTodo"/>
+    <button @click="() => modalOpen = !modalOpen">Create todo</button>
   </main>
 </template>
-
-<style scoped>
-</style>
