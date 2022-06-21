bec472cef6d09b6270101207ce91c6115a3284e7
Kenramiscal1106 <ramiscalken@gmail.com>
2022-06-21 20:51:20 +0800

    changed styling of div container - added animation for modal

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -3,10 +3,21 @@
 </script>
 
 <template>
-  <div class="absolute top-0 w-full h-full bg-white bg-opacity-5 backdrop-blur-sm grid place-content-center">
+  <div class="fixed top-0 left-0 w-full h-full bg-white bg-opacity-40 backdrop-blur-sm grid place-content-center modal z-10">
     <slot></slot>
   </div>
 </template> 
 
-<style>
+<style lang="scss" scoped>
+  @keyframes fade-in {
+    from {
+      opacity:0;
+    }
+    to {
+      opacity:1;
+    }
+  }
+  .modal {
+    animation: fade-in 100ms forwards;
+  }
 </style>
