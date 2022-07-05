b704a36a190d65a8ac45ff25e89b1564fbc34299
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2022-07-05 23:37:46 +0800

    - added sfc

---
+++ src/components/Label.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+interface LabelObject {
+  hueSat: string,
+  text: string,
+}
+const props = defineProps<LabelObject>()
+</script>
+<template>
+  <div class="px-1 flex gap-1 rounded-md items-center text-sm" :style="`background:hsl(${props.hueSat}, 90%)`">
+    <div class="w-4 h-4 rounded-[50%]" :style="`background:hsl(${props.hueSat}, 50%)`"></div>
+    {{ props.text }}
+  </div>
+</template>
+<style scoped>
+</style>
