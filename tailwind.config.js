6c5f6fa231551b6ec63332ed1bb0d74e0b9d5fe5
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2022-07-07 10:13:32 +0800

    - added added color extends

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      textColor: {
+        "secondary": "hsl(0,0,0)",
+        "secondary-light": "hsl(0,0%,40%)",
+        "primary": "hsl(0,0%,100%)",
+        "primary-dark": "hsl(0,0,80%)",
+      },
+      fontSize: {
+        "sub": "0.9rem"
+      }
+    },
   },
   plugins: [],
 };
