c2285b6da1b8d56faeebb78d6391de0b9c85c48c
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2022-07-07 10:51:07 +0800

    - added v-if to element - added button to close modal

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -37,9 +37,19 @@ window.addEventListener("click", () => contextOpen.value = false)
         <div>
           {{specificTodoItem.text}}
         </div>
-        <div class="text-secondary-light text-sub" >
+        <div class="text-secondary-light text-sub" v-if="specificTodoItem.deadline.length !== 0">
           Due in {{specificTodoItem.deadline}}
         </div>
+      </div>
+      <div class="flex items-center ml-auto">
+        <button @click="props.todos.value = props.todos.value.filter(todo => todo.id !== specificTodoItem.id)"
+          class="p-1 border-red-500 border-2 text-red-500  rounded-md hover:text-white hover:bg-red-500 active:bg-red-700"
+        >
+          <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M2.68306 3.56694C2.43898 3.32286 2.43898 2.92714 2.68306 2.68306C2.92714 2.43898 3.32287 2.43898 3.56694 2.68306L10 9.11612L16.4331 2.68306C16.6771 2.43898 17.0729 2.43898 17.3169 2.68306C17.561 2.92714 17.561 3.32286 17.3169 3.56694L10.8839 10L17.3169 16.4331C17.561 16.6771 17.561 17.0729 17.3169 17.3169C17.0729 17.561 16.6771 17.561 16.4331 17.3169L10 10.8839L3.56694 17.3169C3.32287 17.561 2.92714 17.561 2.68306 17.3169C2.43898 17.0729 2.43898 16.6771 2.68306 16.4331L9.11612 10L2.68306 3.56694Z" fill="currentColor"/>
+          </svg>
+
+        </button>
       </div>
       <!-- <div class="">
         <span v-if="specificTodoItem.deadline !== ''"
