30e1647caf02033f5ccb45adadac0c8bdbaf308e
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2022-07-21 13:56:57 +0800

    refactor(components): :recycle: add props and template change

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,14 +1,16 @@
 <script setup lang='ts'>
-  
+const { time } = defineProps<{ time: string }>()
 </script>
 <template>
   <aside class="flex-grow-0 flex-shrink-0 basis-64 bg-primary-light dark:bg-secondary-light space-y-2 px-4 py-3">
-    <h1></h1>
-    <div class="space-y-1">
+    <div class="flex items-center gap-3">
+      <h3 class="font-semibold">Categories</h3>
+      <h4 class="font-medium">{{ time }}</h4>
+    </div>
+    <div class="space-y-1 flex flex-col items-stretch">
       <slot></slot>
     </div>
   </aside>
 </template>
 <style scoped>
-  
 </style>
