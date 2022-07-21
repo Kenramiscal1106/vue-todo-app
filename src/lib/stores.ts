a2ff6d21905229d5537e4cc34fde3713c2597216
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2022-07-21 16:02:42 +0800

    feat(data): :poop: write a test code to be refactored

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,5 +1,6 @@
 import { defineStore } from "pinia";
 import type { Todo, Categories } from "./utils";
+import {v4 as randomUUID} from "uuid"
 export const useTodoStore = defineStore("todos", {
   state: () => ({
     value: [] as Todo[],
@@ -9,6 +10,26 @@ export const useTodoStore = defineStore("todos", {
 
 export const useCategoryStore = defineStore("categories", {
   state: () => ({
-    value: [] as Categories[]
+    value: [{
+      id: randomUUID(),
+      title: "Important",
+      color: "blue"
+    },{
+      id: randomUUID(),
+      title: "Important",
+      color: "blue"
+    },{
+      id: randomUUID(),
+      title: "Important",
+      color: "blue"
+    },{
+      id: randomUUID(),
+      title: "Important",
+      color: "blue"
+    },{
+      id: randomUUID(),
+      title: "Important",
+      color: "blue"
+    }] as Categories[]
   })
 })
