a73ead61dce2834fda90b9381621927b8963e806
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2022-07-21 13:34:56 +0800

    refactor(components): :art: change template

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,7 +2,9 @@
   
 </script>
 <template>
-  
+  <main class="flex-1 px-3 py-4 dark:bg-secondary">
+    <slot></slot>
+  </main>
 </template>
 <style scoped>
   
--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -2,7 +2,12 @@
   
 </script>
 <template>
-  
+  <aside class="flex-grow-0 flex-shrink-0 basis-64 bg-primary-light dark:bg-secondary-light space-y-2 px-4 py-3">
+    <h1></h1>
+    <div class="space-y-1">
+      <slot></slot>
+    </div>
+  </aside>
 </template>
 <style scoped>
   
