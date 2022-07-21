452098c0230ccd0be4e367fae86f8f25b50d5e9e
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2022-07-21 11:05:22 +0800

    feat(utils): add getter (currentTime)

--- src/lib/get.ts
+++ src/lib/get.ts
@@ -1,3 +1,5 @@
+import type { Date } from "./utils"
+
 export function useLocalStorage(key:string) {
   const value = localStorage.getItem(key)
   const set = function(newValue:any) {localStorage.setItem(key, newValue)}
@@ -6,3 +8,43 @@ export function useLocalStorage(key:string) {
     set
   }
 } 
+
+export function useCurrentTime(): { hour12: Date; hour24: Date } {
+  const time = new Date();
+  const hour12: Date = {
+    hour:
+      time.getHours() > 12
+        ? time.getHours() - 12
+        : time.getHours() === 0
+        ? 12
+        : time.getHours(),
+    minute: time.getMinutes(),
+    seconds: time.getSeconds(),
+    twelveHourSplit: function () {
+      return time.getHours() < 12 ? "AM" : "PM";
+    },
+    timeString: function () {
+      return `${this.hour}:${
+        this.minute < 10 ? `0${this.minute}` : this.minute
+      }:${this.seconds < 10 ? `0${this.seconds}` : this.seconds} ${
+        typeof this.twelveHourSplit !== "undefined"
+          ? this.twelveHourSplit()
+          : ""
+      }`;
+    },
+  };
+  const hour24: Date = {
+    hour: time.getHours(),
+    minute: time.getMinutes(),
+    seconds: time.getSeconds(),
+    timeString: function () {
+      return `${
+        time.getHours() > 12 ? time.getHours() - 12 : time.getHours()
+      }:${time.getMinutes()}:${time.getSeconds()}`;
+    },
+  };
+  return {
+    hour12,
+    hour24,
+  };
+}
