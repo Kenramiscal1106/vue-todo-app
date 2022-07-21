a4bf6ab52e6577248c715950dabaf2a008aa963d
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2022-07-21 11:06:52 +0800

    refactor(data): :art: change property state

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -2,13 +2,13 @@ import { defineStore } from "pinia";
 import type { Todo, Categories } from "./utils";
 export const useTodoStore = defineStore("todos", {
   state: () => ({
-    todos: [] as Todo[],
+    value: [] as Todo[],
   }),
 });
 
 
 export const useCategoryStore = defineStore("categories", {
   state: () => ({
-    categories: [] as Categories[]
+    value: [] as Categories[]
   })
 })
