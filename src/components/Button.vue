f319a1a6693428810f5d99fa08017f1fbac3d323
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2022-07-22 18:25:34 +0800

    feat(components): :sparkles: add button component

---
+++ src/components/Button.vue
@@ -0,0 +1,14 @@
+<script setup lang='ts'>
+  const {accent} = defineProps<{
+    accent: boolean,
+    [key: string]: any
+  }>()
+</script>
+<template>
+  <button :class="['px-2 py-1', {'bg-accent': accent}, {'bg-primary-light dark:bg-secondary-light': !accent}]">
+    <slot></slot>
+  </button>
+</template>
+<style scoped>
+  
+</style>
