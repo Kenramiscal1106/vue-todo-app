81df8afb19b8cf5eb2c33dc1fb544c56d40d3f42
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2022-07-24 20:49:58 +0800

    feat(components): :necktie: add setId to current category value onsubmit, set autocomplete to off

--- src/components/sidebar/CategoryEditor.vue
+++ src/components/sidebar/CategoryEditor.vue
@@ -9,17 +9,17 @@ import { useCategoryStore } from '../../lib/stores';
   const category = useCategoryStore();
   function addCategory(e) {
     const formData = Object.fromEntries(new FormData(e.target).entries());
-    console.log(formData)
     category.addCategory({
       id: generateUUID(),
       color: "",
       ...formData
     })
+    category.setId(category.value[category.value.length - 1].id)
     addCategoryVisible.category = false
   }
 </script>
 <template>
-  <form @submit.prevent="addCategory" class="dark:bg-secondary bg-primary px-2 py-1 rounded-md">
+  <form @submit.prevent="addCategory" class="dark:bg-secondary bg-primary px-2 py-1 rounded-md" autocomplete="off">
     <input type="text" name="title" id="" placeholder="Enter category name..." class="block bg-transparent outline-none text-white">
   </form>
 
