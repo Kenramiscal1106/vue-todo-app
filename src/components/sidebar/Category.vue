ba1ffe0e5b7d4108be1b80d50abf70b365185a8d
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2022-07-24 20:48:21 +0800

    feat(components): :recycle: update logic, refactor

--- src/components/sidebar/Category.vue
+++ src/components/sidebar/Category.vue
@@ -1,8 +1,13 @@
 <script setup lang='ts'>
-  
+import { useCategoryStore } from '../../lib/stores';
+
+  const {isActive} = defineProps<{
+    isActive: boolean
+  }>()
+  const categories = useCategoryStore()
 </script>
 <template>
-  <button class="rounded-md px-2 py-1 text-left hover:bg-primary active:bg-primary-dark dark:hover:bg-secondary dark:active:bg-secondary-dark"><slot></slot></button>
+  <button :class="['rounded-md px-2 py-1 text-left ', {'hover:bg-primary active:bg-primary-dark dark:hover:bg-secondary dark:active:bg-secondary-dark': !isActive, 'bg-primary-dark dark:bg-secondary-dark': isActive }]"><slot></slot></button>
 </template>
 <style scoped>
   
