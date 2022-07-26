7a8ae08c5867ab0b90169f7d60188a6d2a10d05d
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2022-07-26 10:00:19 +0800

    style(components): :lipstick: change overflow to prevent scrolling to blank page

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,8 +2,8 @@
   
 </script>
 <template>
-  <main class="flex-1 px-3 py-4 dark:bg-secondary bg-primary space-y-3">
-    <slot></slot>
+  <main class="flex-1 px-3 py-4 dark:bg-secondary bg-primary space-y-3 overflow-auto">
+      <slot></slot>
   </main>
 </template>
 <style scoped>
--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -2,7 +2,7 @@
 const { time } = defineProps<{ time: string }>()
 </script>
 <template>
-  <aside class="flex-grow-0 flex-shrink-0 basis-64 bg-primary-light dark:bg-secondary-light space-y-2 px-4 py-3">
+  <aside class="flex-grow-0 flex-shrink-0 basis-64 bg-primary-light dark:bg-secondary-light space-y-2 px-4 py-3 overflow-auto">
     <div class="flex items-center gap-3">
       <h3 class="font-semibold">Categories</h3>
       <h4 class="font-medium">{{ time }}</h4>
