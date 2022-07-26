6a05649e6e752c2f894f610b60e60d499927e67b
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2022-07-26 09:32:43 +0800

    feat(components): :sparkles: add remove category logic
    refactored code to encapsulate the entire logic

--- src/components/sidebar/Category.vue
+++ src/components/sidebar/Category.vue
@@ -1,14 +1,26 @@
 <script setup lang='ts'>
-import { useCategoryStore } from '../../lib/stores';
-
-  const {isActive} = defineProps<{
-    isActive: boolean
-  }>()
-  const categories = useCategoryStore()
+import type { Category } from '@lib/utils';
+import { useCategoryStore, useTodoStore } from '../../lib/stores';
+import {trashIcon} from "../icons/index"
+const { isActive, value } = defineProps<{
+  isActive: boolean,
+  value: Category
+}>()
+const categories = useCategoryStore()
+const todos = useTodoStore()
+const removeHandler = function() {
+  todos.removeItemsByCategory(value.id)
+  categories.removeCategory(value.id)/* 
+  categories.setId(categories.value[categories.value.length - 2].id)
+  // categories.setId(categories.value[categories.value.length - 2].id) */
+  
+}
 </script>
 <template>
-  <button :class="['rounded-md px-2 py-1 text-left ', {'hover:bg-primary active:bg-primary-dark dark:hover:bg-secondary dark:active:bg-secondary-dark': !isActive, 'bg-primary-dark dark:bg-secondary-dark': isActive }]"><slot></slot></button>
+  <button
+    :class="['rounded-md px-2 py-1 text-left flex items-center group', { 'hover:bg-primary active:bg-primary-dark dark:hover:bg-secondary dark:active:bg-secondary-dark': !isActive, 'bg-primary-dark dark:bg-secondary-dark': isActive }]">{{
+        value.title
+    }} <button class="ml-auto opacity-0 group-hover:opacity-100 transition-opacity" v-html="trashIcon" @click="removeHandler"></button></button>
 </template>
 <style scoped>
-  
 </style>
