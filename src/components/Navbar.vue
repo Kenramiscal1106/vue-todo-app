3d7cf82fe0de7149c0041a67e1661e8316c0a597
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2022-07-31 16:08:57 +0800

    feat(components): :sparkles: add nav

---
+++ src/components/Navbar.vue
@@ -0,0 +1,12 @@
+<script setup lang='ts'>
+import { useStyleStore } from "../lib/stores";
+
+const style = useStyleStore()
+</script>
+<template>
+  <nav class="md:hidden block dark:bg-secondary-dark p-4">
+    <button @click="style.toggleCollapseVisible()">open sidebar</button>
+  </nav>
+</template>
+<style scoped>
+</style>
