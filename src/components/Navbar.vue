faacf995108e1c8348e621b24f76bc66f704759a
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2022-08-01 09:51:12 +0800

    feat(components): :sparkles: :lipstick: update ui and display current category

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,16 +1,18 @@
 <script setup lang='ts'>
-import { useStyleStore } from "../lib/stores";
+import { useCategoryStore, useStyleStore } from "../lib/stores";
 
-const style = useStyleStore()
+const style = useStyleStore();
+const categoryStore = useCategoryStore();
 </script>
 <template>
-  <nav class="md:hidden block dark:bg-secondary-dark p-3">
+  <nav class="md:hidden dark:bg-secondary-dark p-2 flex items-center space-x-3">
     <button @click="style.toggleCollapseVisible()"
-      class="text-gray-800 hover:text-primary dark:text-secondary-accent dark:hover:text-secondary p-2 dark:hover:bg-secondary-light bg-opacity-5 hover:bg-primary-dark"><svg
-        class="w-5 h-5  " viewBox="0 0 24 24">
+      class="text-gray-800 hover:text-primary dark:text-secondary-accent dark:hover:text-secondary p-2 dark:hover:bg-secondary-light bg-opacity-5 hover:bg-primary-dark rounded-full"><svg
+        class="w-6 h-6  " viewBox="0 0 24 24">
         <path d="M5 6h14M5 12h14M5 18h14" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round">
         </path>
       </svg></button>
+    <div>{{ categoryStore.currentCategory ? categoryStore.currentCategory.title : "No category" }}</div>
   </nav>
 </template>
 <style scoped>
