4e6ee2c1a49c334f62b9707f3c2274ced831cc54
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2022-08-01 09:51:43 +0800

    style(components): :recycle: change class to prevent low contrast button

--- src/components/sidebar/Category.vue
+++ src/components/sidebar/Category.vue
@@ -38,16 +38,18 @@ const handleEditExit = (e: KeyboardEvent) => {
     :class="{ 'hover:bg-primary active:bg-primary-dark dark:hover:bg-secondary dark:active:bg-secondary-dark': !isActive, 'bg-primary-dark dark:bg-secondary-dark': isActive }">{{
         value.title
     }}
-    <button class="text-secondary-accent hover:text-secondary ml-auto opacity-0 group-hover:opacity-100 transition-all"
+    <button
+      class="text-gray-700 hover:text-primary dark:text-secondary-accent dark:hover:text-secondary ml-auto opacity-0 group-hover:opacity-100 transition-all"
       v-html="editIcon" @click="handleEditButton"></button>
-    <button class="text-secondary-accent  hover:text-secondary opacity-0 group-hover:opacity-100 transition-all"
+    <button
+      class="text-gray-700 hover:text-primary dark:text-secondary-accent  dark:hover:text-secondary opacity-0 group-hover:opacity-100 transition-all"
       v-html="trashIcon" @click="removeHandler"></button></button>
   <form v-else autocomplete="off" @submit.prevent="handleEditSubmit" class=' px-2 py-1 rounded-md border-2' :class="{
     'border-red-500': value.title === '',
     'dark:border-secondary border-primary': value.title !== '',
   }">
     <input @keydown="handleEditExit" type="text" name="title" id="" placeholder="Enter category name..."
-      class="block bg-transparent outline-none text-white" v-model="titleEdit" ref="input" />
+      class="block bg-transparent outline-none dark:text-white" v-model="titleEdit" ref="input" />
   </form>
 </template>
 <style scoped>
--- src/components/sidebar/CategoryEditor.vue
+++ src/components/sidebar/CategoryEditor.vue
@@ -1,36 +1,36 @@
 <script setup lang='ts'>
-import { v4 as generateUUID} from 'uuid';
+import { v4 as generateUUID } from 'uuid';
 import { onMounted, ref } from 'vue';
 import { useCategoryStore } from '../../lib/stores';
-  const {addCategoryVisible} = defineProps<{
-    addCategoryVisible: {
-      category: boolean
-    }
-  }>()
-  const input = ref<any>(null)
-  onMounted(() => {
-    input.value.focus()
-  })
-  const category = useCategoryStore();
-  function addCategory(e:any) {
-    const formData = Object.fromEntries(new FormData(e.target).entries()) as {
-      title:string
-    }
-    category.addCategory({
-      id: generateUUID(),
-      color: "",
-      ...formData
-    })
-    category.setId(category.value[category.value.length - 1].id)
-    addCategoryVisible.category = false
+const { addCategoryVisible } = defineProps<{
+  addCategoryVisible: {
+    category: boolean
   }
+}>()
+const input = ref<any>(null)
+onMounted(() => {
+  input.value.focus()
+})
+const category = useCategoryStore();
+function addCategory(e: any) {
+  const formData = Object.fromEntries(new FormData(e.target).entries()) as {
+    title: string
+  }
+  category.addCategory({
+    id: generateUUID(),
+    color: "",
+    ...formData
+  })
+  category.setId(category.value[category.value.length - 1].id)
+  addCategoryVisible.category = false
+}
 </script>
 <template>
   <form @submit.prevent="addCategory" class="dark:bg-secondary bg-primary px-2 py-1 rounded-md" autocomplete="off">
-    <input type="text" name="title" id="" placeholder="Enter category name..." class="block bg-transparent outline-none text-white" ref="input" />
+    <input type="text" name="title" id="" placeholder="Enter category name..."
+      class="block bg-transparent outline-none text-primary dark:text-white" ref="input" />
   </form>
 
 </template>
 <style scoped>
-  
 </style>
