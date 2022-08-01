7945c35b9c6e2c2358cd817c24b372bbe8002a14
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2022-08-01 09:48:38 +0800

    chore: :art:

--- src/components/sidebar/AddCategory.vue
+++ src/components/sidebar/AddCategory.vue
@@ -2,12 +2,13 @@
 
 </script>
 <template>
-  <button class="rounded-md flex items-center gap-1 px-2 py-1 bg-accent hover:bg-accent-hover text-left text-white group">
-    <svg width="20" height="20" viewBox="0 0 20 20" class="group-hover:scale-125"
-      fill="none" xmlns="http://www.w3.org/2000/svg">
+  <button
+    class="rounded-md flex items-center gap-1 px-2 py-1 bg-accent hover:bg-accent-hover text-left text-white group">
+    <svg width="20" height="20" viewBox="0 0 20 20" class="group-hover:scale-125" fill="none"
+      xmlns="http://www.w3.org/2000/svg">
       <path fill-rule="evenodd" clip-rule="evenodd"
         d="M10 2.5C10.3452 2.5 10.625 2.77982 10.625 3.125V9.375H16.875C17.2202 9.375 17.5 9.65482 17.5 10C17.5 10.3452 17.2202 10.625 16.875 10.625H10.625V16.875C10.625 17.2202 10.3452 17.5 10 17.5C9.65482 17.5 9.375 17.2202 9.375 16.875V10.625H3.125C2.77982 10.625 2.5 10.3452 2.5 10C2.5 9.65482 2.77982 9.375 3.125 9.375H9.375V3.125C9.375 2.77982 9.65482 2.5 10 2.5Z"
-        fill="currentColor"/>
+        fill="currentColor" />
     </svg>
     <slot></slot>
   </button>
