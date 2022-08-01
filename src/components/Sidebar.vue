1eb841dc02d05e763edc0bcd1c02ea0c78160c48
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2022-08-01 09:27:45 +0800

    fix: :lipstick: media query breakpoints in class

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -5,13 +5,13 @@ const { time } = defineProps<{ time: string }>()
 const style = useStyleStore()
 </script>
 <template>
-  <div class="sm:absolute sm:h-full sm:w-full sm:top-0 md:hidden bg-secondary-dark bg-opacity-40"
-    :class="{ 'sm:hidden': !style.collapseVisible }" @click="style.toggleCollapseVisible">
+  <div class="absolute h-full w-full top-0 md:hidden bg-secondary-dark bg-opacity-40"
+    :class="{ 'hidden': !style.collapseVisible }" @click="style.toggleCollapseVisible">
 
   </div>
   <aside
-    class="sm:absolute sm:w-64 sm:top-0 h-full md:static flex-grow-0 flex-shrink-0 basis-64 bg-primary-light dark:bg-secondary-light space-y-2 px-4 py-3 overflow-auto"
-    :class="{ 'sm:-left-full': !style.collapseVisible }">
+    class="absolute w-64 top-0 h-full md:static flex-grow-0 flex-shrink-0 basis-64 bg-primary-light dark:bg-secondary-light space-y-2 px-4 py-3 overflow-auto"
+    :class="{ '-left-full': !style.collapseVisible }">
     <div class="flex items-center gap-3">
       <h3 class="font-semibold">Categories</h3>
       <h4 class="font-medium">{{ time }}</h4>
