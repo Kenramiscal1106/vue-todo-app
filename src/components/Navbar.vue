a4543fb21fdb65100e827a68db908a172142e2bf
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2022-08-01 09:32:05 +0800

    fix(icon): :bug: add viewBox to fix display issue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -6,8 +6,8 @@ const style = useStyleStore()
 <template>
   <nav class="md:hidden block dark:bg-secondary-dark p-3">
     <button @click="style.toggleCollapseVisible()"
-      class="text-gray-800 hover:text-primary dark:text-secondary-accent dark:hover:text-secondary p-2 dark:hover:bg-primary-light bg-opacity-5 hover:bg-secondary-dark"><svg
-        class="w-5 h-5  ">
+      class="text-gray-800 hover:text-primary dark:text-secondary-accent dark:hover:text-secondary p-2 dark:hover:bg-secondary-light bg-opacity-5 hover:bg-primary-dark"><svg
+        class="w-5 h-5  " viewBox="0 0 24 24">
         <path d="M5 6h14M5 12h14M5 18h14" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round">
         </path>
       </svg></button>
