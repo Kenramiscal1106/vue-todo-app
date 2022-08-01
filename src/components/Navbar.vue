6f45519ecbd281a64f458737dbac56c3e7ff7d9e
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2022-08-01 09:26:57 +0800

    feat(components): :sparkles: change styling and button content to svg

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,8 +4,13 @@ import { useStyleStore } from "../lib/stores";
 const style = useStyleStore()
 </script>
 <template>
-  <nav class="md:hidden block dark:bg-secondary-dark p-4">
-    <button @click="style.toggleCollapseVisible()">open sidebar</button>
+  <nav class="md:hidden block dark:bg-secondary-dark p-3">
+    <button @click="style.toggleCollapseVisible()"
+      class="text-gray-800 hover:text-primary dark:text-secondary-accent dark:hover:text-secondary p-2 dark:hover:bg-primary-light bg-opacity-5 hover:bg-secondary-dark"><svg
+        class="w-5 h-5  ">
+        <path d="M5 6h14M5 12h14M5 18h14" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round">
+        </path>
+      </svg></button>
   </nav>
 </template>
 <style scoped>
